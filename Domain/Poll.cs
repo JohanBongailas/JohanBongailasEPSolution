71d98eb1f3aa6cb3e3a56cbf8b3da78f844883b7
Johan Bongailas <johan.bongailas.e22072@mcast.edu.mt>
2025-04-05 22:38:32 +0200

    refactor(domain): add default values to counts

--- Domain/Poll.cs
+++ Domain/Poll.cs
@@ -18,9 +18,9 @@ namespace Domain
         public string Option2Text { get; set; } = string.Empty!;
         public string Option3Text { get; set; } = string.Empty!;
 
-        public int Option1VotesCount { get; set; }
-        public int Option2VotesCount { get; set; }
-        public int Option3VotesCount { get; set; }
+        public int Option1VotesCount { get; set; } = 0;
+        public int Option2VotesCount { get; set; } = 0;
+        public int Option3VotesCount { get; set; } = 0;
         public DateTime DateCreated { get; set; } = DateTime.UtcNow;
     }
 }
