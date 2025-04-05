4cfac3a51e28af20782aecfa9456bd555905e398
Johan Bongailas <johan.bongailas.e22072@mcast.edu.mt>
2025-04-05 21:48:03 +0200

    refactor(domain): mark string properties as non-nullable

--- Domain/Poll.cs
+++ Domain/Poll.cs
@@ -11,10 +11,13 @@ namespace Domain
     {
         [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Option1Text { get; set; }
-        public string Option2Text { get; set; }
-        public string Option3Text { get; set; }
+
+        // Making the string properties non-nullable
+        public string Title { get; set; } = string.Empty!;
+        public string Option1Text { get; set; } = string.Empty!;
+        public string Option2Text { get; set; } = string.Empty!;
+        public string Option3Text { get; set; } = string.Empty!;
+
         public int Option1VotesCount { get; set; }
         public int Option2VotesCount { get; set; }
         public int Option3VotesCount { get; set; }
