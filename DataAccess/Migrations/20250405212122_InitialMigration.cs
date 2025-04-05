3b965d25d3d5fc7ce4fe689ca557bd2144602bb1
Johan Bongailas <johan.bongailas.e22072@mcast.edu.mt>
2025-04-05 23:24:28 +0200

    Added the migration

---
+++ DataAccess/Migrations/20250405212122_InitialMigration.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Polls",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Option1Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Option2Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Option3Text = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Option1VotesCount = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    Option2VotesCount = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    Option3VotesCount = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETUTCDATE()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Polls", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Polls");
+        }
+    }
+}
