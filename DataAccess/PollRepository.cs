fd1895a9e4bdf925f6f44ee87e47457583fc44d9
Johan Bongailas <johan.bongailas.e22072@mcast.edu.mt>
2025-04-05 19:32:28 +0200

    Add project files.

---
+++ DataAccess/PollRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    /// <summary>
+    /// Handles CRUD operations for Poll entities. 
+    /// </summary>
+    class PollRepository
+    {
+    }
+}
