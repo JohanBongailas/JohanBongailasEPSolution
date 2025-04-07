f818ee61f0927c2de0290f63b7c33128975d0e25
Johan Bongailas <johan.bongailas.e22072@mcast.edu.mt>
2025-04-07 20:15:40 +0200

    feat{DataAccess}: Add CreatePoll action in PollRepository.

--- DataAccess/PollRepository.cs
+++ DataAccess/PollRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,5 +12,29 @@ namespace DataAccess
     /// </summary>
     public class PollRepository
     {
+        // Inject the DbContext instance using DI to interact with the database
+        private readonly PollDbContext _context;
+        public PollRepository(PollDbContext context)
+        {
+            _context = context;
+        }
+        public void CreatePoll(string title, string option1Text, 
+                               string option2Text, string option3Text, 
+                               int option1VotesCount, int option2VotesCount, int option3VotesCount)
+        {
+
+            _context.Polls.Add(new() 
+            {
+                Title = title,
+                Option1Text = option1Text,
+                Option2Text = option2Text,
+                Option3Text = option3Text,
+                Option1VotesCount = option1VotesCount,
+                Option2VotesCount = option2VotesCount,
+                Option3VotesCount = option3VotesCount,
+            });
+
+            _context.SaveChanges();
+        }
     }
 }
