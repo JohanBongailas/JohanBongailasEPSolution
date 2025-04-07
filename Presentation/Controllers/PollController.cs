c18db3c7bdad7810efaab36b0c94171b3c80ad0f
Johan Bongailas <johan.bongailas.e22072@mcast.edu.mt>
2025-04-07 21:08:51 +0200

    feat{Controller}: Add action method to add the poll in the database and another action method to render the form and register PollRepository in program.cs.

--- Presentation/Controllers/PollController.cs
+++ Presentation/Controllers/PollController.cs
@@ -1,10 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers
 {
     public class PollController : Controller
     {
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreatePoll([FromServices] PollRepository pollRepository, string title, string option1Text, string option2Text, string option3Text,
+                                        int option1VotesCount = 0, int option2VotesCount = 0, int option3VotesCount = 0)
+        {
+            try
+            {
+                // Use the PollRepository to create and save the poll
+                pollRepository.CreatePoll(title, option1Text,
+                                          option2Text, option3Text,
+                                          option1VotesCount, option2VotesCount, option3VotesCount);
+
+                TempData["Message"] = "Poll created successfully!";
+            } catch (Exception ex)
+            {
+                // Handle any exceptions that occur during the poll creation
+                ModelState.AddModelError("", $"An error occurred while creating the poll: {ex.Message}");
+                return View("Index");
+            }
+
+
+
+            // Redirect to index page
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult CreatePollForm()
         {
             return View();
         }
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -19,6 +19,7 @@ public class Program
         builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             .AddEntityFrameworkStores<PollDbContext>();
         builder.Services.AddControllersWithViews();
+        builder.Services.AddScoped<PollRepository>();
 
         var app = builder.Build();
 
